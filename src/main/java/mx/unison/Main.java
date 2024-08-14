a122ffdde83fe5583cb0887e34495271896c87a8
Sebastian Duarte Quijada <a223205136@unison.mx>
2024-08-14 00:47:26 -0700

    Código final

--- src/main/java/mx/unison/Main.java
+++ src/main/java/mx/unison/Main.java
@@ -4,9 +4,36 @@ package mx.unison;
 y cantos a asentamientos rurales
  */
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
+        String archivo = "codigos_postales.csv";
+        String lineas = "";
+        String division = ",";
 
-        System.out.println("Hello world!");
+        int Urbano = 0;
+        int Rural = 0;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+            while ((lineas = br.readLine()) != null) {
+                String[] datos = lineas.split(division);
+                String Asentamiento = datos[2].trim();
+
+                if (Asentamiento.equalsIgnoreCase("Urbano")) {
+                    Urbano++;
+                } else if (Asentamiento.equalsIgnoreCase("Rural")) {
+                    Rural++;
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Número de asentamientos urbanos: " + Urbano);
+        System.out.println("Número de asentamientos rurales: " + Rural);
     }
 }
